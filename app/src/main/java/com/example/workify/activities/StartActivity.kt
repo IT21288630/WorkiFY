4c844c36f13cd62ed6217e86ede4c7b91bc28c72
Nilusha Wijeratne <99312650+IT21264184@users.noreply.github.com>
2023-05-06 15:30:54 +0530

    cus management

--- app/src/main/java/com/example/workify/activities/StartActivity.kt
+++ app/src/main/java/com/example/workify/activities/StartActivity.kt
@@ -1,16 +1,12 @@
 package com.example.workify.activities
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.workify.R
 
-class MainActivity : AppCompatActivity() {
+class StartActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        var intent = Intent(this, CustomerLoginActivity2::class.java)
-        startActivity(intent)
+        setContentView(R.layout.activity_start)
     }
 }
