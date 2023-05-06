2b2a53153c0718c14e36d7a61dccb818787f00e2
IT21288630 <it21288630@my.sliit.lk>
2023-05-06 15:35:09 +0530

    Worker Management

--- app/src/main/java/com/example/workify/activities/StartActivity.kt
+++ app/src/main/java/com/example/workify/activities/StartActivity.kt
@@ -1,12 +1,27 @@
 package com.example.workify.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.example.workify.R
 
 class StartActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start)
+
+        val hireBtn = findViewById<Button>(R.id.hireBtn)
+        val workBtn = findViewById<Button>(R.id.workBtn)
+
+        hireBtn.setOnClickListener {
+            val intent = Intent(this@StartActivity, CustomerLoginActivity2::class.java)
+            startActivity(intent)
+        }
+
+        workBtn.setOnClickListener {
+            val intent = Intent(this@StartActivity, WorkerLoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
