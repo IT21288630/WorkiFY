b3e3b2398bebeae0c606c9451f916b46eee7db90
Nilusha Wijeratne <99312650+IT21264184@users.noreply.github.com>
2023-05-06 15:20:32 +0530

    cus management

---
+++ app/src/main/java/com/example/workify/activities/CustomerLoginActivity2.kt
@@ -0,0 +1,83 @@
+package com.example.workify.activities
+
+import android.app.Person
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import com.example.workify.R
+import com.example.workify.dataClasses.Worker
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.ktx.toObject
+import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
+
+class CustomerLoginActivity2 : AppCompatActivity() {
+
+    private val workerCollectionRef = Firebase.firestore.collection("customers")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_customer_login2)
+
+        val etWEmail = findViewById<EditText>(R.id.etWEmail)
+        val etWPassword = findViewById<EditText>(R.id.etWPassword)
+        val workerLoginBtn = findViewById<ImageView>(R.id.workerLoginBtn)
+        val workerSignUp = findViewById<TextView>(R.id.workerSignUp)
+
+        workerSignUp.setOnClickListener {
+            val intent = Intent(this@CustomerLoginActivity2, CustomerSignupActivity::class.java)
+            startActivity(intent)
+        }
+
+        workerLoginBtn.setOnClickListener {
+            val email = etWEmail.text.toString()
+            val password = etWPassword.text.toString()
+
+            workerLogin(email, password)
+        }
+    }
+
+    private fun workerLogin(email: String, password: String) =
+        CoroutineScope(Dispatchers.IO).launch {
+            try {
+                val querySnapshot = workerCollectionRef
+                    .whereEqualTo("email", email)
+                    .whereEqualTo("password", password)
+                    .get()
+                    .await()
+
+                if (querySnapshot.isEmpty) {
+                    withContext(Dispatchers.Main) {
+                        Toast.makeText(
+                            this@CustomerLoginActivity2,
+                            "Wrong credentials",
+                            Toast.LENGTH_LONG
+                        ).show()
+                    }
+                }
+
+                for (document in querySnapshot.documents) {
+                    val worker = document.toObject<Worker>()
+
+                    if (worker != null) {
+                        var intent = Intent(this@CustomerLoginActivity2, CustomerActivity::class.java)
+                        intent.putExtra("curWorkerEmail", email)
+                        startActivity(intent)
+                    }
+                }
+
+            } catch (e: Exception) {
+                withContext(Dispatchers.Main) {
+                    Toast.makeText(this@CustomerLoginActivity2, e.message, Toast.LENGTH_LONG).show()
+                }
+            }
+        }
+}
---
+++ app/src/main/java/com/example/workify/activities/CustomerSignupActivity.kt
@@ -0,0 +1,61 @@
+package com.example.workify.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.Toast
+import com.example.workify.R
+import com.example.workify.dataClasses.Customer
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
+
+class CustomerSignupActivity : AppCompatActivity() {
+
+    private val workerCollectionRef = Firebase.firestore.collection("customers")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_customer_signup)
+
+        val etWName = findViewById<EditText>(R.id.etWName)
+        val etWEmail = findViewById<EditText>(R.id.etWEmail)
+        val etWDistrict = findViewById<EditText>(R.id.etWDistrict)
+        val etWPassword = findViewById<EditText>(R.id.etWPassword)
+        val workerRegBtn = findViewById<ImageView>(R.id.workerRegBtn)
+
+        workerRegBtn.setOnClickListener {
+            val name = etWName.text.toString()
+            val email = etWEmail.text.toString()
+            val district = etWDistrict.text.toString()
+            val password = etWPassword.text.toString()
+
+
+            val customer = Customer(name, email, district, password)
+
+            saveWorker(customer)
+        }
+
+
+    }
+
+    private fun saveWorker(worker: Customer) = CoroutineScope(Dispatchers.IO).launch {
+        try {
+            workerCollectionRef.add(worker).await()
+
+            withContext(Dispatchers.Main) {
+                Toast.makeText(this@CustomerSignupActivity, "Customer added", Toast.LENGTH_LONG).show()
+            }
+
+        } catch (e: Exception) {
+            withContext(Dispatchers.Main) {
+                Toast.makeText(this@CustomerSignupActivity, e.message, Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+}
