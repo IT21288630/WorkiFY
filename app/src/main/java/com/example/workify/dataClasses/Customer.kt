9d9845ed9ebe0af671c64fd473556e77408d0d9c
Nilusha Wijeratne <99312650+IT21264184@users.noreply.github.com>
2023-05-06 15:21:29 +0530

    cus management

---
+++ app/src/main/java/com/example/workify/dataClasses/Customer.kt
@@ -0,0 +1,8 @@
+package com.example.workify.dataClasses
+
+data class Customer(
+    var name: String = "",
+    var email: String = "",
+    var district: String = "",
+    var password: String = "",
+)
